a42751dbb389df65481f0a5dd81c7cc8e4fa6ac9
Firzenax <louis.20160437@gmail.com>
2024-03-20 17:35:55 +0100

    fix routing

--- rentingapp_front/src/App.tsx
+++ rentingapp_front/src/App.tsx
@@ -1,27 +1,29 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Outlet, Route, Routes } from "react-router-dom";
 import "./App.css";
 import LandingPage from "./Pages/LandingPage";
 import RentingPage from "./Pages/RentingPage";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/home",
-      element: <LandingPage />,
-    },
-    {
-      path: "/rent",
-      element: <RentingPage />,
-    },
-  ]);
-
   return (
     <>
-      <Navbar />
-      <RouterProvider router={router} />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<LandingPage />} />
+          <Route path="rent" element={<RentingPage />} />
+        </Route>
+      </Routes>
     </>
   );
 }
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
+};
 
 export default App;
