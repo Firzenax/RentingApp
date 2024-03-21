271cfff07ee7b754594ed774f999d3410c4cdd56
Firzenax <louis.20160437@gmail.com>
2024-03-21 17:07:41 +0100

    manage cross origin

--- rentingapp/src/main/java/com/codinglouis/rentingapp/controllers/BrandController.java
+++ rentingapp/src/main/java/com/codinglouis/rentingapp/controllers/BrandController.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173/")
 @RequestMapping("/brands")
 public class BrandController {
     private final BrandRepository brandRepository;
